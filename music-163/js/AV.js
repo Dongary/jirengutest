9ae52235c19e2ed3e846bc52b0ec72a070511ac8
donegary <991537024@qq.com>
2018-08-19 01:32:15 +0800

    增加管理数据库页面、实现存入上传音乐文件以及存储相关信息到数据库的可视化操作

---
+++ music-163/js/AV.js
@@ -0,0 +1,12 @@
+{
+    let APP_ID = 'Q97xE3Obe1yJm90H7f1cnhKc-gzGzoHsz';
+    let APP_KEY = '26KJu1WVsR1CqmxUNrssE50R';
+    let {
+        Query,
+        User
+    } = AV;
+    AV.init({
+        appId: APP_ID,
+        appKey: APP_KEY
+    });
+}
